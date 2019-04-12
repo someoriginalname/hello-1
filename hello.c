b08d2b36ff8f17a352f43c4c85cecd470f1cd614
XioSC <albastroiudragos@gmail.com>
2019-04-12 08:33:42 +0300

    Now takes the name from command line as an argument

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	printf("Salut, Alex!\n");
+	if (argc!=2)
+	{
+		printf("Mod de folosire: %s nume\n", argv[0]);
+		return -1;
+	}
+	printf("Salut, %s!\n", argv[1]);
 	return 0;
 }
